238146e364df23f78fa1f1bd9e954d7a232b662b
shin-fued <wachirawit.yea@student.mahidol.com>
2025-04-02 23:21:40 +0700

    my tsne is so fucking garbage

--- my_tsne.py
+++ my_tsne.py
@@ -3,16 +3,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import euclidean_distances
 
-class my_TSNE:
-    def __init__(self, learning, moment):
-        self.learning = learning
-        self.moment = moment
+#this garbage is for benchmarking purposes
 
+class my_TSNE:
+    def __init__(self, learning):
+        self.learning = learning
+
+    def symmetric_sne_grad(self, P, Y):
+        Q = self.prob_low_dim(Y)
+        pq_diff = P - Q  # NxN matrix
+        pq_expanded = np.expand_dims(pq_diff, 2)  # NxNx1
+        y_diffs = np.expand_dims(Y, 1) - np.expand_dims(Y, 0)  # NxNx2
+        grad = 4. * (pq_expanded * y_diffs).sum(1)  # Nx2
+        return grad
 
     def prob_high_dim(self, sigma, dist_row, dist):
-        exp_dist = np.exp(-dist[dist_row]/2*sigma**2)
+        exp_dist = exp_dist = np.exp(-dist[dist_row] / (2 * sigma**2))
         exp_dist[dist_row] = 0
-        prob_not_sym = exp_dist/np.sum(exp_dist)
+        prob_not_sym = exp_dist/np.sum(exp_dist) + 1e-10
         return prob_not_sym
 
     def perplexity(self, prob):
@@ -37,37 +45,48 @@ class my_TSNE:
         np.fill_diagonal(inv_distances, 0.)
         return inv_distances / np.sum(inv_distances, axis=1, keepdims=True)
 
-    def KL(self, P, Q):
+    def KL(self, P, Y):
+        Q = self.prob_low_dim(Y)
         return P * np.log(P+0.01) - P * np.log(Q+0.01)
 
     def KL_grad(self, P, Y):
         Q = self.prob_low_dim(Y)
-        ydiff = np.expand_dims(Y, axis=1) - np.expand_dims(Y, axis=1)
-        inv_dist = np.power(1 + np.square(ydiff), -1)
+        y_diff = np.expand_dims(Y, 1) - np.expand_dims(Y, 0)
+        inv_dist = np.power(1 + np.square(euclidean_distances(Y, Y)), -1)
+        pq_diff = np.expand_dims(P - Q, axis=2)  # (n, n, 1)
+        inv_dist = np.expand_dims(inv_dist, axis=2)  # (n, n, 1)
+        return 4 * np.sum(pq_diff * y_diff * inv_dist, axis=1)
 
     def get_sigma(self):
         return self.sigma_array
 
+    def get_KLs(self):
+        return self.KL_array
 
+    def get_KL(self):
+        return self.KL_array[-1]
 
-
-    def fit(self, X, Y, Perplexity, d, Iter):
+    def fit(self, X, Perplexity, d, Iter):
         X = np.log(X + 1)
         dist = np.square(euclidean_distances(X, squared=True))
         n = X.shape[0]
         prob = np.zeros((n, n))
         sigma_array = []
         for dist_row in range(n):
-            func = lambda sigma: self.perplexity(self.prob_high_dim(sigma, dist_row))
+            func = lambda sigma: self.perplexity(self.prob_high_dim(sigma, dist_row, dist))
             binary_search_result = self.sigma_binary_search(func, Perplexity)
-            prob[dist_row] = self.prob_high_dim(binary_search_result, dist_row)
+            prob[dist_row] = self.prob_high_dim(binary_search_result, dist_row, dist)
             sigma_array.append(binary_search_result)
         self.sigma_array = sigma_array
-        P = prob + np.transpose(prob)
-        Q = self.prob_low_dim(Y)
-        y = np.random.normal(loc = 0, scale = 1e4, size=(n, d))
+        P = (prob + np.transpose(prob)) / 2
+        np.random.seed(12345)
+        Y = np.random.normal(loc = 0, scale = 1e4, size=(n, d))
         KL_array = []
-
-
+        for i in range(Iter):
+            y_grad = self.symmetric_sne_grad(P, Y)
+            Y = Y - self.learning * y_grad
+            KL_array.append(np.sum(self.KL(P, Y)))
+        self.KL_array = KL_array
+        return Y
 
 
--- my_umap.py
+++ my_umap.py
@@ -1,13 +1,46 @@
+import numpy as np
+import pandas as pd
 from scipy import optimize
 import matplotlib.pyplot as plt
-import numpy as np
+from sklearn.manifold import SpectralEmbedding
 from sklearn.metrics.pairwise import euclidean_distances
 
 class my_UMAP:
-    def __init__(self, n_components=2):
-        self.n_components = n_components
-        self.a = p[0]
-        b = p[1]
+    def __init__(self, learning):
+        self.learning = learning
+
+    def get_prob(self, A, x_i, x_j, pi, sigma_i, knn):
+        if x_j in knn[x_i]:
+            return np.exp(-(np.linalg.norm(A[x_i] - A[x_j]) - pi) / sigma_i)
+        return 0
+
+    def bin_search(self, arr, goal, epsilon):
+        start, end = 0, len(arr) - 1
+        for i in range(end + 1):
+            middle = int((end - start) / 2)
+            if goal - epsilon <= arr[middle] <= goal + epsilon:
+                return middle
+            elif arr[middle] < goal - epsilon:
+                start = middle + 1
+            else:
+                end = middle - 1
+        return -1
+
+    def find_sigma_i(self, x_i, pi: float, k: int, Neighbours, A: np.ndarray):
+        sig_i = np.linspace(0, 10, 100)
+        goal = np.log2(k)
+
+        def sig_exp(x_i, pi, sig, Neighbours, A):
+            res = 0
+            for x_ij in Neighbours:
+                res += np.exp(-(np.linalg.norm(A[x_i] - A[x_ij]) - pi) / sig)
+            return res
+
+        sigs = sig_exp(x_i, pi, sig_i, Neighbours, A)
+        sig = self.bin_search(sigs, goal, 0.01)
+        return sig_i[sig]
+
+
 
     def prob_low_dim(Y):
         """
@@ -17,19 +50,12 @@ class my_UMAP:
         np.fill_diagonal(inv_distances, 0.)
         return inv_distances / np.sum(inv_distances, axis=1, keepdims=True)
 
-    def prob_low_dim(Y):
-        """
-        Compute matrix of probabilities q_ij in low-dimensional space
-        """
+    def prob_low_dim(self, Y):
         inv_distances = np.power(1 + a * np.square(euclidean_distances(Y, Y)) ** b, -1)
         return inv_distances
 
-    def CE(P, Y):
-        """
-        Compute Cross-Entropy (CE) from matrix of high-dimensional probabilities
-        and coordinates of low-dimensional embeddings
-        """
-        Q = prob_low_dim(Y)
+    def Cross_Entropy(P, Y):
+        Q = self.prob_low_dim(Y)
         return - P * np.log(Q + 0.01) - (1 - P) * np.log(1 - Q + 0.01)
 
     def CE_gradient(P, Y):
